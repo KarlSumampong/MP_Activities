2a36403a1d50ad5adf5e4fb84a94831aea330187
KarlSumampong <kfsumampong@mcm.edu.ph>
2025-12-12 20:09:38 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,17 @@
+console.log('Before binding event');
+
+// bind
+let txtInput = document.querySelector('#txtInput');
+let btnReplaceAll = document.querySelector('#btnReplaceAll');
+let txtOutput = document.querySelector('#txtOutput');
+
+
+// event
+btnReplaceAll.addEventListener('click',function(){
+    // logic
+    let str = txtInput.value;
+    let result = str.replaceAll(" ","");
+    // txtOutput?
+    txtOutput.innerText = result;
+    console.log(result);
+});
